2fdd516782e295cc93a207bb01f8ac633734a66e
Yasmin Gonçalves <yasmingoncalves5421@gmail.com>
2024-06-03 23:07:16 -0300

    feat: tags

--- Python/database/script.sql
+++ Python/database/script.sql
@@ -8,8 +8,10 @@ create table tbl_viagem(
     qtd_dias int not null,
     qtd_noites int not null,
     avaliacao_media float,
+    imagem text not null,
     unique index(id)
 );
+
 
 create table tbl_viagem_avaliacao (
 	id int not null auto_increment primary key,
@@ -18,7 +20,30 @@ create table tbl_viagem_avaliacao (
     
     constraint fk_viagem_viagem_avaliacao
     foreign key (id_viagem)
-    references tbl_viagem_avaliacao (id),
+    references tbl_viagem (id),
+    
+    unique index (id)
+);
+
+create table tbl_tag (
+	id int not null auto_increment primary key,
+    nome varchar(100) not null,
+    
+    unique index (id)
+);
+
+create table tbl_tag_viagem (
+	id int not null auto_increment primary key,
+	id_viagem int not null,
+    id_tag int not null,
+    
+    constraint fk_viagem_tag_viagem
+    foreign key (id_viagem)
+    references tbl_viagem (id),
+    
+    constraint fk_tag_tag_viagem
+    foreign key (id_tag)
+    references tbl_tag (id),
     
     unique index (id)
 );
