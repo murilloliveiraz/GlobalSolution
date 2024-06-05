728da04e97075754d6e816d3137b12dca2a16cf5
Yasmin Gonçalves <yasmingoncalves5421@gmail.com>
2024-06-04 23:30:22 -0300

    add: preco e avaliacao no front

--- FrontendAndWebDev/js/main.js
+++ FrontendAndWebDev/js/main.js
@@ -11,7 +11,7 @@ const criarCards = (viagem) => {
         <div class="card-text">
             <div class="rating">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFF55"><path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-350Z"/></svg>
-                <p>5 <em>(123)</em></p>
+                <p>${viagem.avaliacao_media} <em>(123)</em></p>
             </div>
             <h3>${viagem.nome}</h3>
             <div>
@@ -20,7 +20,7 @@ const criarCards = (viagem) => {
                     </span>
                 <p>${viagem.qtd_dias} dias e ${viagem.qtd_noites} noites</p>
             </div>
-            <p>R$ 899.00</p>
+            <p>R$ ${viagem.preco}</p>
         </div>
         <button>Reservar</button>
     </article>
